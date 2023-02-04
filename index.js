6b6657b7b616db1e6896febb3917c8fc02122718
TheLoneliestTime <john.a.roxas@gmail.com>
2023-02-03 16:10:19 -0800

    wrote the functionality for the sum function which sums all nubmers from x to y

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
-function sum(fromN, toN) {
+function sum(fromN, toN, tracker = 0, total = 0) {
   // Sum all the values from fromN up to toN
+
+  //base case
+  if (toN - fromN - tracker < 0) {
+    return total;
+  } else {
+    return sum(fromN, toN, tracker + 1, total = total + (fromN + tracker));
+  }
+
 }
+
+// console.log(sum(3, 7));
 
 module.exports = sum;
